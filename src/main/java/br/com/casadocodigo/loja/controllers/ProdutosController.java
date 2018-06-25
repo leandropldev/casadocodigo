3c89be9f9f829a241a88c5ff3dd665ee95bbbfc7
leandropldev <developer.reis@gmail.com>
2018-06-25 20:08:09 -0300

    iniciando projeto

---
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -0,0 +1,53 @@
+	package br.com.casadocodigo.loja.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import br.com.casadocodigo.loja.daos.ProdutoDAO;
+import br.com.casadocodigo.loja.models.Produto;
+import br.com.casadocodigo.loja.models.TipoPreco;
+
+@Controller
+@RequestMapping("produtos")
+public class ProdutosController {
+	
+	@Autowired
+	private ProdutoDAO produtoDAO;
+
+	@RequestMapping("/form")
+	public ModelAndView form(){
+		ModelAndView modelAndView = new ModelAndView("produtos/form");
+		modelAndView.addObject("tipos", TipoPreco.values());
+		
+		return modelAndView;
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ModelAndView gravar(Produto produto, RedirectAttributes redirectAttributes){
+		
+		if(produto.getTitulo().isEmpty()){
+			System.out.println("Titulo vazio");
+			System.out.println("Titulo vazio");
+			return form();
+		}
+		produtoDAO.gravar(produto);
+		redirectAttributes.addFlashAttribute("sucesso", "Produto cadastrado com sucesso!");
+		
+		return new ModelAndView("redirect:produtos");
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ModelAndView listar(){
+		List<Produto> produtos = produtoDAO.listar();
+		ModelAndView modelAndView = new ModelAndView("produtos/lista");
+		modelAndView.addObject("produtos", produtos);
+		
+		return modelAndView;
+	}
+}
