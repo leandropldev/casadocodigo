e7c0ef2678d1f99b719311f384466dbec029731e
Leandro <lreis@coddera.com>
2018-07-16 10:58:47 -0300

    aula 09  send files

--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -2,26 +2,42 @@
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.casadocodigo.loja.daos.ProdutoDAO;
+import br.com.casadocodigo.loja.infra.FileSaver;
 import br.com.casadocodigo.loja.models.Produto;
 import br.com.casadocodigo.loja.models.TipoPreco;
+import br.com.casadocodigo.loja.validation.ProdutoValidation;
 
 @Controller
-@RequestMapping("produtos")
+@RequestMapping("/produtos")
 public class ProdutosController {
 	
 	@Autowired
 	private ProdutoDAO produtoDAO;
-
+	
+	@Autowired
+	private FileSaver fileSaver;
+	
+	@InitBinder
+	public void initBinder(WebDataBinder binder){
+		binder.addValidators(new ProdutoValidation());
+	}
+	
 	@RequestMapping("/form")
-	public ModelAndView form(){
+	public ModelAndView form(Produto produto){
 		ModelAndView modelAndView = new ModelAndView("produtos/form");
 		modelAndView.addObject("tipos", TipoPreco.values());
 		
@@ -29,17 +45,20 @@ public class ProdutosController {
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
-	public ModelAndView gravar(Produto produto, RedirectAttributes redirectAttributes){
+	public ModelAndView gravar(MultipartFile sumario, @Valid Produto produto, BindingResult result, RedirectAttributes redirectAttributes){
 		
-		if(produto.getTitulo().isEmpty()){
-			System.out.println("Titulo vazio");
-			return form();
+		if(result.hasErrors()){
+			return form(produto);
 		}
+		
+		String path = fileSaver.write("arquivo-sumario", sumario);
+		produto.setSumarioPath(path);
+		
 		produtoDAO.gravar(produto);
 		redirectAttributes.addFlashAttribute("sucesso", "Produto cadastrado com sucesso!");
 		
 		return new ModelAndView("redirect:produtos");
-	}
+	}	
 	
 	@RequestMapping(method=RequestMethod.GET)
 	public ModelAndView listar(){
--- src/main/java/br/com/casadocodigo/loja/models/Produto.java
+++ src/main/java/br/com/casadocodigo/loja/models/Produto.java
@@ -1,5 +1,6 @@
 package br.com.casadocodigo.loja.models;
 
+import java.util.Calendar;
 import java.util.List;
 
 import javax.persistence.ElementCollection;
@@ -7,6 +8,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 public class Produto {
@@ -15,11 +19,18 @@ public class Produto {
 	private int id;
 	
 	private String titulo;
+	
+	@Lob
 	private String descricao;
 	private int paginas;
 	
+	@DateTimeFormat
+	private Calendar dataLancamento;
+	
 	@ElementCollection
 	private List<Preco> precos;
+	
+	private String sumarioPath;
 	
 	public int getId() {
 		return id;
@@ -32,6 +43,12 @@ public class Produto {
 	}
 	public void setPrecos(List<Preco> precos) {
 		this.precos = precos;
+	}
+	public Calendar getDataLancamento() {
+		return dataLancamento;
+	}
+	public void setDataLancamento(Calendar dataLancamento) {
+		this.dataLancamento = dataLancamento;
 	}
 	public String getTitulo() {
 		return titulo;
@@ -55,6 +72,12 @@ public class Produto {
 	@Override
 	public String toString() {
 		return "Produto [titulo=" + titulo + ", descricao=" + descricao + ", paginas=" + paginas + "]";
+	}
+	public String getSumarioPath() {
+		return sumarioPath;
+	}
+	public void setSumarioPath(String sumarioPath) {
+		this.sumarioPath = sumarioPath;
 	}
 	
 	
