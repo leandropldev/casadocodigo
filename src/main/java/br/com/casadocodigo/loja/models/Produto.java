782ce98c4ef5d9aba2b86c5f817fcd23d4c0696c
Leandro <lreis@coddera.com>
2018-07-20 16:34:38 -0300

    modulo 1 finalizado

--- src/main/java/br/com/casadocodigo/loja/models/Produto.java
+++ src/main/java/br/com/casadocodigo/loja/models/Produto.java
@@ -1,5 +1,6 @@
 package br.com.casadocodigo.loja.models;
 
+import java.math.BigDecimal;
 import java.util.Calendar;
 import java.util.List;
 
@@ -78,6 +79,30 @@ public class Produto {
 	}
 	public void setSumarioPath(String sumarioPath) {
 		this.sumarioPath = sumarioPath;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	public BigDecimal precoPara(TipoPreco tipoPreco) {
+		return precos.stream().filter(preco -> preco.getTipo().equals(tipoPreco)).findFirst().get().getValor();
+		
 	}
 	
 	
